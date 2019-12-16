68f6288c59389f6cbb919b925495d4dffe5bbf6b
Krzysztof Szpieg <kszpieg@gmail.com>
2019-12-16 14:21:38 +0100

    Create methods in DatabaseAccess which get data about Meals and Ingredients from db

--- app/src/main/java/com/example/noshapp/DatabaseAccess.java
+++ app/src/main/java/com/example/noshapp/DatabaseAccess.java
@@ -5,6 +5,11 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import com.example.noshapp.models.Ingredients;
+import com.example.noshapp.models.Meal;
+
+import java.util.ArrayList;
+
 public class DatabaseAccess {
 
     private SQLiteOpenHelper openHelper;
@@ -13,39 +18,67 @@ public class DatabaseAccess {
     private final String MEAL_TABLE = "MEALS";
     private final String INGREDIENTS_TABLE = "INGREDIENTS";
 
-    private DatabaseAccess(Context context){
+    private DatabaseAccess(Context context) {
         this.openHelper = new DatabaseOpenHelper(context);
     }
 
-    public static DatabaseAccess getInstance(Context context){
-        if(instance==null){
+    public static DatabaseAccess getInstance(Context context) {
+        if (instance == null) {
             instance = new DatabaseAccess(context);
         }
         return instance;
     }
 
-    public void open(){
+    public void open() {
         this.db = openHelper.getWritableDatabase();
     }
 
-    public void close(){
-        if(db!=null){
+    public void close() {
+        if (db != null) {
             this.db.close();
         }
     }
 
-    public Cursor viewMealListData(){
+    public Cursor viewMealListData() {
         open();
         String query = "Select * from " + MEAL_TABLE;
-        Cursor cursor = db.rawQuery(query,null );
+        Cursor cursor = db.rawQuery(query, null);
 
         return cursor;
     }
 
-    public Cursor getIngredientsForMeal(int mealID){
+    public ArrayList<Meal> getMealsFromDb() {
+        ArrayList<Meal> meals = new ArrayList<>();
+        open();
+        String query = "Select * from " + MEAL_TABLE;
+        Cursor c = db.rawQuery(query, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            if (c.getString(c.getColumnIndex("Name")) != null) {
+                Meal meal = new Meal(c.getString(c.getColumnIndex("Name")), getIngredientsForMeal(c.getColumnIndex("ID")), c.getInt(c.getColumnIndex("Portion")), c.getString(c.getColumnIndex("Description")));
+                meals.add(meal);
+            }
+            c.moveToNext();
+        }
+        close();
+        return meals;
+    }
+
+    public ArrayList<Ingredients> getIngredientsForMeal(int mealID) {
+        ArrayList<Ingredients> ingredients = new ArrayList<>();
         open();
         String query = "Select * from " + INGREDIENTS_TABLE + " where Meal_ID = " + mealID;
-        Cursor cursor = db.rawQuery(query,null);
-        return cursor;
+        Cursor c = db.rawQuery(query, null);
+        c.moveToFirst();
+        while (!c.isAfterLast()) {
+            if (c.getString(c.getColumnIndex("Name")) != null) {
+                Ingredients ingredient = new Ingredients(c.getString(c.getColumnIndex("Name")), c.getInt(c.getColumnIndex("Quantity")), c.getString(c.getColumnIndex("Unit")), c.getInt(c.getColumnIndex("Meal_ID")));
+                ingredients.add(ingredient);
+            }
+            c.moveToNext();
+        }
+        close();
+        return ingredients;
     }
+
 }
--- app/src/main/java/com/example/noshapp/ShowMealActivity.java
+++ app/src/main/java/com/example/noshapp/ShowMealActivity.java
@@ -2,8 +2,16 @@ package com.example.noshapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.noshapp.models.Meal;
+
+import java.util.ArrayList;
 
 public class ShowMealActivity extends AppCompatActivity {
 
@@ -12,6 +20,9 @@ public class ShowMealActivity extends AppCompatActivity {
     TextView tvMealPortions;
     TextView tvMealDescription;
     TextView tvMealDescriptionTitle;
+
+    ArrayList<Meal> mealsFromDb = new ArrayList<>();
+    DatabaseAccess db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,34 @@ public class ShowMealActivity extends AppCompatActivity {
         tvMealDescription = findViewById(R.id.tvMealDescription);
         tvMealDescriptionTitle = findViewById(R.id.tvMealDescriptionTitle);
 
+        Intent myCallerIntent = getIntent();
+        Bundle bundle = myCallerIntent.getExtras();
 
+        if (bundle == null) {
+            displayDefaultTexts();
+        } else {
+            String mealNameFromList = bundle.getString("mealName");
+            showMeal(mealNameFromList);
+        }
+    }
+
+    private void showMeal(String mealNameFromList) {
+        db = DatabaseAccess.getInstance(getApplicationContext());
+        db.open();
+        mealsFromDb = db.getMealsFromDb();
+        String targetMealName = mealNameFromList;
+        Meal targetMeal = null;
+        for (Meal meal : mealsFromDb)
+        {
+            if(targetMealName.equals(meal.getName())){
+                targetMeal = meal;
+            }
+        }
+        tvMealName.setText(targetMeal.getName());
+        tvMealIngredients.setText("");
+        tvMealPortions.setText(targetMeal.getPortions());
+        tvMealDescription.setText(targetMeal.getDescription());
+        tvMealDescriptionTitle.setText("Opis");
     }
 
     private void displayDefaultTexts() {
